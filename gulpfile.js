bf9a4e8ee0748eae08261addfc61db984e5290c6
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-06-21 16:24:01 +0700

    Add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp");
 const sourcemaps = require("gulp-sourcemaps");
 const ts = require("gulp-typescript");
-const gulpWebpack = require("webpack-stream");
+const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
-const clean = require('gulp-dest-clean');
+const watch = require("gulp-watch");
+const sequence = require('gulp-watch-sequence');
+const clean = require("gulp-dest-clean");
+const plumber = require('gulp-plumber');
 
 const webpackConfig = require("./scripts/webpack-config.js");
 
@@ -19,6 +22,7 @@ const outDir = targetDir(distDir);
 
 const tsTask = (debug) => () => {
   return gulp.src(`${srcDir}/**/*.ts`)
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(tsProject())
     .pipe(sourcemaps.write(".", {includeContent: false, sourceRoot: `../../${srcDir}`}))
@@ -27,12 +31,24 @@ const tsTask = (debug) => () => {
 
 const webpackTask = (debug) => () => {
   return gulp.src(`${tsOutDir(debug)}/**/*.js`)
+    .pipe(plumber())
     .pipe(clean(outDir(debug)))
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(gulpWebpack(webpackConfig(debug, tsOutDir(debug)), webpack))
-    .pipe(sourcemaps.write(".", {includeContent: false, sourceRoot: `../../${srcDir}`}))
+    .pipe(webpackStream(webpackConfig(debug, tsOutDir(debug)), webpack))
     .pipe(gulp.dest(outDir(debug)));
+};
+
+const watchTask = (debug) => () => {
+  var queue = sequence(300);
+  watch(`${srcDir}/**/*.ts`, {
+    name: "TS",
+    emitOnGlob: false
+  }, queue.getHandler(`build:${debug}`));
+  watch(`${tsOutDir(debug)}/**/*.ts`, {
+    name: "JS",
+    emitOnGlob: false
+  }, queue.getHandler(`build:${debug}`));
 };
 
 gulp.task("ts:debug", tsTask("debug"));
 gulp.task("build:debug", ["ts:debug"], webpackTask("debug"));
+gulp.task("watch:debug", watchTask("debug"));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { sprintf } from 'sprintf-js'
 
-console.log(sprintf("Hello, %s!", "world"))
+console.log(sprintf("Hello, %s!!!", "world"))
