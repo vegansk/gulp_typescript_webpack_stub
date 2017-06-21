6b63a7a6b9602531abde3da11a141915ea069f69
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-06-21 22:05:05 +0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { sprintf } from 'sprintf-js'
 import './style/style.css'
 
+if (process.env.NODE_ENV === "development") {
+  console.log("Starting debug version...")
+} else {
+  console.log("Starting release version...")
+}
+
 console.log(sprintf("Hello, %s", "world!"))
